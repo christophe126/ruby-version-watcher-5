b4b5085d46d3e6028294c33525e67853db724107
christophe126 <christophe126@gmail.com>
2022-05-24 16:13:50 +0200

    whenever gem

--- app/models/version.rb
+++ app/models/version.rb
@@ -7,14 +7,24 @@ class Version < ApplicationRecord
     CSV.generate(headers: true) do |csv|
       csv << attributes
 
-      all.each do |user|
-        csv << attributes.map{ |attr| user.send(attr) }
+      all.each do |version|
+        csv << aversion.attributes.values_at(*attributes)
       end
     end
   end
 
-  def name
-    "#{first_name} #{last_name}"
+   def check_new_version_ruby
+    result = []
+    mechanize = Mechanize.new
+    read_data = mechanize.get('https://www.ruby-lang.org/fr/news/')
+    first_div_post = read_data.search('div.post').first
+    if first_div_post.search('p').first.text[0..3] == "Ruby"
+      ruby_version = first_div_post.search('p').first.text.strip.split[1]
+      ruby_date = first_div_post.search('p').last.text.strip.split[4]
+      result = [ruby_version, ruby_date]
+    end
+    return result
   end
+
 
 end
